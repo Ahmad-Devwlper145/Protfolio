74d5874d8b7cef6fccda5cd53e19f217c4bb5e82
Ahmad Genius <ahmad.develper125@gmail.com>
2026-08-01 00:14:50 +0500

    Fix Minor Bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
             "@react-three/rapier",
             "@react-three/postprocessing",
           ],
-          gsap: ["gsap", "gsap-trial", "@gsap/react"],
+          gsap: ["gsap", "@gsap/react"],
           react: ["react", "react-dom"],
         },
       },
